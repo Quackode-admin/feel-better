68a679f33b33b81f6902a9b3bd76ec7175f702d4
doug.sanchez32 <doug.sanchez32@gmail.com>
2026-05-13 16:24:02 -0600

    fix: simplify AppModule for initial deploy

--- services/api-core/src/app.module.ts
+++ services/api-core/src/app.module.ts
@@ -1,32 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseModule } from './database/database.module'
-import { AuthModule } from './auth/auth.module'
-import { UsersModule } from './users/users.module'
-import { PatientsModule } from './patients/patients.module'
-import { NutritionModule } from './nutrition/nutrition.module'
-import { AppointmentsModule } from './appointments/appointments.module'
-import { TrackingModule } from './tracking/tracking.module'
-import { ChatModule } from './chat/chat.module'
-import { FilesModule } from './files/files.module'
 
 @Module({
   imports: [
-    // Carga variables de entorno globalmente
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
-
-    // Infraestructura
-    DatabaseModule,
-
-    // Dominio
-    AuthModule,
-    UsersModule,
-    PatientsModule,
-    NutritionModule,
-    AppointmentsModule,
-    TrackingModule,
-    ChatModule,
-    FilesModule,
   ],
 })
 export class AppModule {}
